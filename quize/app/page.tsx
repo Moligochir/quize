732f51a7424f666464e6d313ae407c7d20fa65ac
25LP5143 25LP5143 <25LP5143@G305-CC0240.local>
2025-12-11 12:35:19 +0800

    prisma

--- quize/app/page.tsx
+++ quize/app/page.tsx
@@ -1,65 +1,152 @@
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
+import { FileText, RotateCw, Sparkles, Trash, User2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { SideBar } from "./_components/SideBar";
+
+type DetectedObject = {
+  label: string;
+  score: number;
+  box: { xmin: number; ymin: number; xmax: number; ymax: number };
+};
 
 export default function Home() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [objects, setObjects] = useState<DetectedObject[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSelectedFile(file);
+      setImagePreview(URL.createObjectURL(file));
+      setObjects([]);
+    }
+  };
+
+  const handleDetect = async () => {
+    if (!selectedFile) return;
+    setLoading(true);
+    setObjects([]);
+
+    try {
+      const formData = new FormData();
+      formData.append("image", selectedFile);
+
+      const data = await (
+        await fetch("/api/object-derection", {
+          method: "POST",
+          body: formData,
+        })
+      ).json();
+
+      setObjects(data.object || []);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const removeSelectedImage = () => {
+    setSelectedFile(null);
+
+    if (imagePreview) {
+      URL.revokeObjectURL(imagePreview);
+    }
+    setImagePreview("");
+  };
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="w-full  flex-col justify-end items-end ">
+      <div className="flex w-full justify-between items-center pl-12 pr-12">
+        <h1 className="p-4  text-xl font-bold">AI tools</h1>
+        <Button variant="outline" size="icon" className="rounded-full w-10 ">
+          <User2 />
+        </Button>
+      </div>
+      <hr className="w-full" />
+
+      <div className="flex w-full justify-center h-full items-start">
+        {" "}
+        <SideBar />
+        <div className="flex w-full max-w-[580px] flex-col gap-6">
+          <Card className="border-none shadow-none ">
+            <CardHeader>
+              <CardTitle>
+                <div className="flex w-full h-fit justify-between">
+                  <div className="flex gap-2 justify-center items-center text-lg font-bold">
+                    <Sparkles />
+                    Article Quiz Generator
+                  </div>
+                  {/* <Button
+                    className="w-12 h-10"
+                    variant="outline"
+                    size="icon"
+                    aria-label="Submit"
+                  >
+                    <RotateCw />
+                  </Button> */}
+                </div>
+              </CardTitle>
+              <CardDescription>
+                Paste your article below to generate a summarize and quiz
+                question. Your articles will saved in the sidebar for future
+                reference.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-6">
+              <div className="grid gap-3">
+                <label className="flex">
+                  <FileText />
+                  Article Title
+                </label>
+                <Input
+                  className="h-10"
+                  id="tabs-demo-name"
+                  placeholder="Enter a title for your article..."
+                  accept="image/*" // Restrict file selection to images
+                  onChange={handleImageUpload}
+                />
+              </div>
+              <div className="grid gap-3">
+                <label className="flex">
+                  <FileText />
+                  Article Title
+                </label>
+                <Input
+                  className="h-10"
+                  id="tabs-demo-name"
+                  placeholder="Enter a title for your article..."
+                  accept="image/*" // Restrict file selection to images
+                  onChange={handleImageUpload}
+                />
+              </div>
+              <div className="flex justify-end">
+                <Button
+                  onClick={handleDetect}
+                  disabled={!imagePreview}
+                  className="h-10"
+                >
+                  {loading ? "Generating..." : "Generate"}
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div className="flex w-full px-10 justify-end items-end  absolute bottom-10"></div>
     </div>
   );
 }
